277e4057b17efbec60a5c629b72fe8ebb49078b5
teewhy-source <128053927+teewhy-source@users.noreply.github.com>
2023-09-12 00:46:25 +0100

    updated

--- meal.js
+++ meal.js
@@ -1,19 +1,37 @@
-let userinput= document.querySelector("#input")
-let display = document.querySelector(".span-2")
-// let input2= document.querySelector("#2")
-userinput.addEventListener('change',(e)=> {
-    e.preventDefault()
-    console.log(userinput.value);
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${userinput.value}`)
-    .then(respond => {
-        return respond.json()
-    }).then(data => {
-        console.log(data.meals)
-        // console.log(data.meals[0].strMeal)
-        // console.log(data.meals[0].strMealThumb);
-        // console.log(data.meals[0].strYoutube);
-        // console.log(data.meals[0].strInstructions);
-        // console.log(data.meals[0].strSource);
-     display.innerText = data.meals[0].strInstructions;
+let userinput = document.querySelector("#input");
+let display = document.querySelector(".span-2");
+let display2 = document.querySelector(".span-3");
+userinput.addEventListener("change", (e) => {
+  e.preventDefault();
+
+  const inputValue = userinput.value;
+
+  // Check if the input value is not empty
+  if (!inputValue) {
+    console.log("Please enter a valid input.");
+    return;
+  }
+
+  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
     })
-})
+    .then((data) => {
+      console.log(data.meals);
+
+      if (data.meals && data.meals.length > 0) {
+        // Assuming the API response contains meals
+        display.innerText = data.meals[0].strInstructions;
+        // display2.innerHTML= <img src={data.meals.strMealThumb} alt="" srcset=""></img>
+      } else {
+        display.innerText = "No matching meals found.";
+        
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
+});
